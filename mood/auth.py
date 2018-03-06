6d9a8b05524929098d61ad12e1a130551c73f0a5
adamredwoods <adamredwoods@users.noreply.github.com>
2018-03-06 09:28:05 -0800

    signup verification

--- mood/auth.py
+++ mood/auth.py
@@ -5,20 +5,31 @@ from django.http import HttpResponse
 
 # Auth-related routes
 def signup(request):
-    print(333)
+
     if request.method == "GET":
-        return render(request, "./signup.html")
+        return render(request, "./signup.html", {"error":""})
     elif request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        firstname = request.POST["firstname"]
-        email = request.POST["email"]
-        try:
-            user = User.objects.create_user(username=username, password=password, first_name=firstname,email=email)
-            if user is not None:
-                return login(request)
-        except:
-            return render(request, "./signup.html", {"error": "Signup failed. User exists?"})
+        if (request.POST["password"] !="" and
+            request.POST["username"] !="" and
+            request.POST["email"] !="" and
+            request.POST["password"] == request.POST["password2"]):
+
+            username = request.POST["username"]
+            password = request.POST["password"]
+            firstname = request.POST["firstname"]
+            email = request.POST["email"]
+            try:
+                user = User.objects.create_user(username=username, password=password, first_name=firstname,email=email)
+                if user is not None:
+                    return login(request)
+            except:
+                return render(request, "./signup.html", {"error": "Signup failed. User exists?"})
+        else:
+            msg ="Please fill out the fields."
+            if (request.POST["password"] != request.POST["password2"]): msg="Password verify does not match."
+            return render(request, "./signup.html", {
+                "error": msg,"username": request.POST["username"], "email": request.POST["email"], "firstname": request.POST["firstname"]
+            })
 
         return HttpResponse("Posted to signup")
 

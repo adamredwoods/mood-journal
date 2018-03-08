4b7b0b618f25a658f9d8743cf5e816c3fbc2fe77
adamredwoods <adamredwoods@users.noreply.github.com>
2018-03-07 19:45:38 -0800

    heroku whitenoise

--- moodjournal/settings.py
+++ moodjournal/settings.py
@@ -46,7 +46,9 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -130,3 +132,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_HOST = os.environ.get('DJANGO_STATIC_HOST', '')
+STATIC_ROOT = STATIC_HOST + os.path.join(BASE_DIR, '/static/')
